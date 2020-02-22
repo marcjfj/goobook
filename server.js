3c7753fc3ec317bb0996f5556540bb325facfb58
Marc <marcjfj@gmail.com>
2020-02-21 22:29:15 -0600

    initial

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'build')));
+
 }
 
 const { Schema } = mongoose;
